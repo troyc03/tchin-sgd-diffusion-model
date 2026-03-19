9bb2dcc325dddc9de0d650d798035680a114bd18
Troy Chin <tchin@csu.fullerton.edu>
2026-03-19 12:44:18 -0700

    v10.0 - Scrapped original code for the analysis and experiments modules. Will be refactoring later.

--- src/analysis/parameter_diffusion.py
+++ src/analysis/parameter_diffusion.py
@@ -2,7 +2,5 @@ import numpy as np
 import torch
 
 class ParameterDiffusionAnalyzer:
-    def __init__(self, model, data_loader, loss_fn):
-       self.model = model
-       self.data_loader = data_loader
-       self.loss_fn = loss_fn
+    # Insert parameters here.
+    pass
--- src/experiments/measure_diffusion.py
+++ src/experiments/measure_diffusion.py
@@ -3,17 +3,5 @@ from models.cnn import SimpleCNN
 import torch
 
 def measure_diffusion(model, data_loader, num_iterations=100):
-    """
-    Measure the diffusion properties of the model's parameter space by analyzing the Hessian matrix.
-    """
-    hessian_analyzer = Hessian(model, loss_fn, data_loader)
-    hessian_matrix = hessian_analyzer.compute_hessian()
-    eigenvalues = hessian_analyzer.compute_spectra(hessian_matrix)
-    hessian_analyzer.plot_spectra(eigenvalues)
-
-if __name__ == "__main__":
-    # Example usage
-    model = SimpleCNN()
-    loss_fn = torch.nn.CrossEntropyLoss()
-    data_loader = ...
-    measure_diffusion(model, data_loader)
+    # Insert parameters here.
+    pass
